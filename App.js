de90486ad36e2b4f8af05ba250c38f16fb1854f6
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-11-04 02:37:58 +0100

    - feat: add config

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from './src/navigation/StackNavigator';
+import { NotifierWrapper } from 'react-native-notifier';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <NotifierWrapper>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </NotifierWrapper>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
+import {Notifier, NotifierComponents} from 'react-native-notifier';
 
 // API for sending videos
 export const muxBaseUrl = 'https://api.mux.com';
@@ -21,4 +22,45 @@ export const imageTypeSize = 'thumbnail.jpg?time=5&width=200';
 // Content Type used in API calls
 export const contentType = 'application/json';
 
-export const fetcher = url => axios.get(url).then(res => res.data);
+export const fetcher = url =>
+  axios.get(url).then(res => {
+    console.log(res.headers);
+    return res.data;
+  });
+
+export const notify = {
+  notify(title: string, description: string, type: string) {
+    Notifier.showNotification({
+      title,
+      description,
+      Component: NotifierComponents.Alert,
+      componentProps: {
+        alertType: type,
+      },
+    });
+  },
+  error(title: string, description?: string) {
+    notify.notify(title, description, 'error');
+  },
+  success(title: string, description?: string) {
+    notify.notify(title, description, 'success');
+  },
+};
+
+export function getFormattedDate(timestamp) {
+  const date = new Date(timestamp * 1000);
+
+  return (
+    date.getDate() +
+    '/' +
+    (date.getMonth() + 1) +
+    '/' +
+    date.getFullYear() +
+    ' ' +
+    date.getHours() +
+    ':' +
+    date.getMinutes() +
+    ':' +
+    date.getSeconds()
+  );
+}
